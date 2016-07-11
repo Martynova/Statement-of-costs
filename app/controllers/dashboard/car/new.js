b124d0c1679ee9e2e5004f01433d24a2ac68bf35
Ira Martynova <Ira Martynova>
2016-07-12 01:36:34 +0300

    create form car with pikaday

--- app/controllers/dashboard/car/new.js
+++ app/controllers/dashboard/car/new.js
@@ -4,9 +4,11 @@ export default Ember.Controller.extend({
 	actions: {
     save(model) {
       console.log('+--- save action called in car new controller');
+      this.transitionToRoute('dashboard.car');
     },
     cancel() {
       console.log('+--- cancel action called in car new controller');
+      this.transitionToRoute('dashboard.car');
     }
   }
 });
