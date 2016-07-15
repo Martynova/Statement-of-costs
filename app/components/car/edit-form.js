08435e21954a61cdbdce6df89e201cd6a57a5166
Ira Martynova <Ira Martynova>
2016-07-15 09:18:41 +0300

    remove tasc when you click cancel

--- app/components/car/edit-form.js
+++ app/components/car/edit-form.js
@@ -2,20 +2,29 @@ import Ember from 'ember';
 import moment from 'moment';
 
 export default Ember.Component.extend({
-  
-  newDate:Ember.computed('model.dataName', function() {
-    console.log('model.dataName',this.get('model.dataName') );
-      return this.get('model.dataName');
-  }),
 
+  newDate: Ember.computed('model.dataName', function() {
+    console.log('model.dataName', this.get('model.dataName'));
+    return this.get('model.dataName');
+  }),
+  isValid: Ember.computed('model.dataName', {
+    get() {
+      return !Ember.isEmpty(this.get('model.dataName'))
+    }
+  }),
   actions: {
+
     save() {
       console.log('+- save action in edit-form component');
-
- 
-      this.get('model').save().then((dashboard)=>{
-        return this.save(dashboard);
-      })
+      if (this.get('isValid')) {
+        this.get('model').save().then((dashboard) => {
+          return this.save(dashboard);
+        }, (err)=> {
+          this.set('errorMessage', 'There was something wrong saving the model');
+        })
+      }else{
+        this.set('errorMessage', 'You have to fill date');
+      }
     },
     cancel() {
       console.log('+- cancel action in edit-form component');
--- app/routes/dashboard/car/new.js
+++ app/routes/dashboard/car/new.js
@@ -1,7 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	model(){
+	model() {
 		return this.store.createRecord('dashboard');
+	},
+	resetController(controller, isExiting) {
+		if (isExiting) {
+			console.log('----- resetController hook called -----');
+			var model = controller.get('model');
+			if(model.get('isNew')){
+				 model.destroyRecord();
+			}
+		}
 	}
 });
